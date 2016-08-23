1859a53057273ea40b712098074581642231f814
arunasank <aruna@mapbox.com>
2016-08-23 14:34:44 +0530

    cleanup

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-//bbox: [-122.05862045288086, 36.93768132842635, -121.97296142578124, 37.00378647456494]
-
-
 'use strict';
 var tileReduce = require('tile-reduce');
 var path = require('path');
-// var argv = require('minimist')(process.argv.slice(2));
-// var mbtilesPath = argv.mbtiles;
-
 
 tileReduce({
-  zoom: 12,
-  map: path.join(__dirname, '/park.js'),
-  sources: [{
-    name: 'osm',
-    mbtiles: '../united_states_of_america.mbtiles',
-    raw: false
-  },
-  {
-    name: 'accidentTiles',
-    mbtiles: '../accident.mbtiles',
-    raw: false
-  }]
+    zoom: 12,
+    map: path.join(__dirname, '/park.js'),
+    sources: [{
+        name: 'osm',
+        mbtiles: '../united_states_of_america.mbtiles',
+        raw: false
+    },
+    {
+        name: 'accidentTiles',
+        mbtiles: '../accident.mbtiles',
+        raw: false
+    }]
 })
-.on('reduce', function(feature) {
-  // if (feature.length > largest.length) {
-  //   largest.length = feature.length;
-  //   largest.feature = feature.feature;
-  // }
-})
-.on('end', function() {
+.on('reduce', function () {
 
-  // write the largest roundabout to stderr
-  // process.stderr.write(JSON.stringify(largest));
+})
+.on('end', function () {
+
 });
--- osmFeaturesInBuffer.js
+++ osmFeaturesInBuffer.js
@@ -1,53 +1,50 @@
 'use strict';
 var turf = require('turf');
 
-module.exports = function(tileLayers, tile, writeData, done) {
-  var largest = {
-    'length': 0,
-    'feature': {}
-  };
-  var osmLayer = tileLayers.osm.osm;
-  var accidents = tileLayers.accidentTiles.accident;
+module.exports = function (tileLayers, tile, writeData, done) {
+    var osmLayer = tileLayers.osm.osm;
+    var accidents = tileLayers.accidentTiles.accident;
 
-  accidents.features.forEach(function(accidentFeature) {
-      accidentFeature.properties = {
-          'state': accidentFeature.properties.state,
-          'crashNum': accidentFeature.properties.crashNum,
-          'fatalities': accidentFeature.properties.fatalities
-      };
-      var accidentBuffer = turf.buffer(accidentFeature, 100, 'meters');
-      var keys = Object.keys(accidentFeature.properties);
-      osmLayer.features.forEach(function(osmFeature) {
-          if (osmFeature.properties.amenity) {
-              var flag = false;
-              switch(osmFeature.geometry.type) {
-                  case 'Point': flag = turf.inside(osmFeature, accidentBuffer);
-                        break;
-                  case 'Polygon': var intersect = turf.intersect(accidentBuffer, osmFeature);
-                         flag = intersect && intersect.geometry.type === 'Polygon' && (turf.area(intersect) >= (turf.area(osmFeature) * 0.5));
-                        break;
-                  case 'LineString': var featureBuffer = turf.buffer(osmFeature, '0.001', 'meters');
-                                     var intersect = turf.intersect(accidentBuffer, featureBuffer);
-                        flag = intersect && intersect.geometry.type === 'Polygon' && (turf.area(intersect) >= (turf.area(featureBuffer) * 0.5));
-                       break;
-              }
-              if (flag) {
-                  if (keys.indexOf(osmFeature.properties.amenity) === -1) {
-                      accidentFeature.properties[osmFeature.properties.amenity + 'Count'] = 0;
-                      accidentFeature.properties[osmFeature.properties.amenity + 'Name'] = [];
-                      accidentFeature.properties[osmFeature.properties.amenity + 'ID'] = [];
-                  } else {
-                      accidentFeature.properties[osmFeature.properties.amenity + 'Count']++;
-                      accidentFeature.properties[osmFeature.properties.amenity + 'Name'].push(osmFeature.properties.name);
-                      accidentFeature.properties[osmFeature.properties.amenity + 'ID'].push(osmFeature.properties['@id']);
-                  }
-              }
-          }
-      });
-  });
+    accidents.features.forEach(function (accidentFeature) {
+        accidentFeature.properties = {
+            'state': accidentFeature.properties.state,
+            'crashNum': accidentFeature.properties.crashNum,
+            'fatalities': accidentFeature.properties.fatalities
+        };
+        var accidentBuffer = turf.buffer(accidentFeature, 100, 'meters');
+        var keys = Object.keys(accidentFeature.properties);
+        osmLayer.features.forEach(function (osmFeature) {
+            if (osmFeature.properties.amenity) {
+              consoel.log('amenity');
+                var flag = false;
+                switch (osmFeature.geometry.type) {
+                case 'Point': flag = turf.inside(osmFeature, accidentBuffer);
+                    break;
+                case 'Polygon': var intersect = turf.intersect(accidentBuffer, osmFeature);
+                    flag = (intersect && intersect.geometry.type === 'Polygon' && (turf.area(intersect) >= (turf.area(osmFeature) * 0.5)));
+                    break;
+                case 'LineString': var featureBuffer = turf.buffer(osmFeature, '0.001', 'meters');
+                    intersect = turf.intersect(accidentBuffer, featureBuffer);
+                    flag = (intersect && intersect.geometry.type === 'Polygon' && (turf.area(intersect) >= (turf.area(featureBuffer) * 0.5)));
+                    break;
+                }
+                if (flag) {
+                    if (keys.indexOf(osmFeature.properties.amenity) === -1) {
+                        accidentFeature.properties[osmFeature.properties.amenity + 'Count'] = 0;
+                        accidentFeature.properties[osmFeature.properties.amenity + 'Name'] = [];
+                        accidentFeature.properties[osmFeature.properties.amenity + 'ID'] = [];
+                    } else {
+                        accidentFeature.properties[osmFeature.properties.amenity + 'Count']++;
+                        accidentFeature.properties[osmFeature.properties.amenity + 'Name'].push(osmFeature.properties.name);
+                        accidentFeature.properties[osmFeature.properties.amenity + 'ID'].push(osmFeature.properties['@id']);
+                    }
+                }
+            }
+        });
+    });
 
-  // write all roundabouts to stdout
-  writeData(JSON.stringify(accidents) + '\n');
+    // write all roundabouts to stdout
+    writeData(JSON.stringify(accidents) + '\n');
 
-  done(null, null);
+    done(null, null);
 };
